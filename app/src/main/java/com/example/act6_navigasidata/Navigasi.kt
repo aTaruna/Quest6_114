0a76be01983bd962dc0294ebcc0d6c381eded90b
aTaruna <aTaruna@users.noreply.github.com>
2025-11-20 13:34:56 +0700

    menambah parameter dan variable

--- app/src/main/java/com/example/act6_navigasidata/Navigasi.kt
+++ app/src/main/java/com/example/act6_navigasidata/Navigasi.kt
@@ -3,25 +3,31 @@ package com.example.act6_navigasidata
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.act5_nav.view.FormIsian
-import com.example.act5_nav.view.TampilData
+import com.example.act6_navigasidata.view.FormIsian
+import com.example.act6_navigasidata.view.TampilData
 
 enum class Navigasi{
     FormulirQue,
     Detail
 }
 
+
 @Composable
 fun DataApp(
-    navController: NavHostController = rememberNavController(),
-    modifier: Modifier
+    modifier: Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+
 ){
     Scaffold { isiRuang ->
+        val uiState = viewModel.statusUI.collectAsState()
         NavHost(
             navController = navController,
             startDestination = Navigasi.FormulirQue.name,
@@ -38,7 +44,8 @@ fun DataApp(
                     onBackBtnClick = {
                         cancelAndBackToFormulirQue(navController = navController)
                     },
-                ) }
+                )
+            }
         }
     }
 }
