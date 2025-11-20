715ca3b6b1c5a37de7584e3b9e53f2a2ab0b2a29
aTaruna <aTaruna@users.noreply.github.com>
2025-11-20 11:53:54 +0700

    copas file Formulir, Navigasi, TampilData

---
+++ app/src/main/java/com/example/act6_navigasidata/Navigasi.kt
@@ -0,0 +1,48 @@
+package com.example.act6_navigasidata
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.act5_nav.view.FormIsian
+import com.example.act5_nav.view.TampilData
+
+enum class Navigasi{
+    FormulirQue,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.FormulirQue.name,
+            modifier = modifier.padding(isiRuang)) {
+            composable(route = Navigasi.FormulirQue.name) {
+                FormIsian(
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name) {
+                TampilData(
+                    onBackBtnClick = {
+                        cancelAndBackToFormulirQue(navController = navController)
+                    },
+                ) }
+        }
+    }
+}
+
+private fun cancelAndBackToFormulirQue(navController: NavHostController) {
+    navController.popBackStack(Navigasi.FormulirQue.name, false)
+}
