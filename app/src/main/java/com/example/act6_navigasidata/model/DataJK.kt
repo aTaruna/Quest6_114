9631abcf792233a2ab915ac6a65da9a04c0de094
aTaruna <aTaruna@users.noreply.github.com>
2025-11-20 12:39:48 +0700

    menambahkan DataJK.kt

---
+++ app/src/main/java/com/example/act6_navigasidata/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.act6_navigasidata.model
+
+import com.example.act6_navigasidata.R
+
+object DataJK {
+    val JenisK = listOf(
+        R.string.laki,
+        R.string.cewek
+    )
+}
